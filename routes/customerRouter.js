d08da3c433015ac5243e61ac5ed812a4725ef814
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2022-12-13 14:14:34 +0100

    Ajout middleware authToken

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -1,11 +1,12 @@
 const CustomerController = require('../controllers/CustomerController');
+const authToken = require('../middleware/authToken');
 module.exports = (app) => {
     app.group("/customers", (router) => {
         router.get("/", CustomerController.getAllCustomers); // FindAll
         router.post('/create', CustomerController.addCustomer); // Create
         router.post('/authenticate', CustomerController.authenticateCustomer); // Authenticate
-        router.patch('/:id', CustomerController.updateCustomer); // Update
-        router.delete('/:id', CustomerController.deleteCustomer); // Delete
+        router.patch('/:id', authToken, CustomerController.updateCustomer); // Update
+        router.delete('/:id', authToken, CustomerController.deleteCustomer); // Delete
         router.get('/:id', CustomerController.getCustomer); // FindOne
     });
 }
