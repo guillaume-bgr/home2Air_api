c0ec1842ab9c058c8c6a08d8cf2ff9cbeb0fdb5f
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2022-12-14 14:47:29 +0100

    Ajout retour token expiré

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,10 +2,20 @@ const jwt = require('jsonwebtoken');
 
 function authToken(req, res, next) {
     let authToken = req.headers && req.headers.authorization;
-    authToken = authToken.split(' ');
-    var decoded = jwt.verify(authToken[1], process.env.SECRET_KEY);
-    res.tokenId = decoded.id;
-    next()
+    if (authToken) {
+        authToken = authToken.split(' ');
+        try {
+            var decoded = jwt.verify(authToken[1], process.env.SECRET_KEY);
+            res.tokenId = decoded.id;
+            res.tokenRole = decoded.role;
+            next()
+          } catch(err) {
+            if(err.name == 'TokenExpiredError'){
+                res.status(500).json({ message: 'Token has expired', err: 'TokenExpiredError' })
+            }
+            next()
+          }
+    }
 }
 
 module.exports = authToken;
