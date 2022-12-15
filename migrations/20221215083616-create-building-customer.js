96f4a9beb92c4d3fc926b358af1cb826e25f9c45
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2022-12-15 09:53:37 +0100

    Changement convention nom building

--- migrations/20221215083616-create-building-customer.js
+++ migrations/20221215083616-create-building-customer.js
@@ -3,12 +3,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Buildings', {
+      customers_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Customers',
+          key: 'id'
+        }
+      },
+      building_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Buildings',
+          key: 'id'
+        }
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
