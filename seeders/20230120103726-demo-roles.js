75e04fa42fa62255c8e7229e7c90570a33c0cf23
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-02-16 11:32:34 +0100

    Ajout seeders

---
+++ seeders/20230120103726-demo-roles.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('Roles', [{
+      name: 'USER',
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
