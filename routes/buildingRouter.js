8d15669a59cb185e042343a0a692c882c9d35b29
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-02-17 10:42:00 +0100

    Ecriture routes building router

--- routes/buildingRouter.js
+++ routes/buildingRouter.js
@@ -4,10 +4,8 @@ module.exports = (app) => {
     app.group("/buildings", (router) => {
         router.get("/", BuildingController.getBuildings); // FindBuildings
         router.get("/:id", BuildingController.getBuilding); // FindBuilding
-        router.get("/user/:id", BuildingController.getCustomerBuildings); // FindUserBuildings
         router.post('/create', authToken, BuildingController.addBuilding); // Create
         router.patch('/:id', authToken, BuildingController.updateBuilding); // Update
-        // router.delete('/:id', BuildingController.deleteCompany); // Delete
-        // router.get('/:id', BuildingController.getCompany); // FindOne
+        router.delete('/:id', authToken, BuildingController.deleteBuilding); // Delete
     });
 }
