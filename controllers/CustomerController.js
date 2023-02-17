ef32081653374dc8bdcab5adc3a7cd80580003a8
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2023-02-17 10:53:37 +0100

    Ajout includes

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,5 +1,9 @@
 const db = require('./../models/index');
 const Customer = db['Customers'];
+const Building = db['Buildings'];
+const Company = db['Companies'];
+const Role = db['Roles'];
+const Ticket = db['Tickets'];
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -8,11 +12,29 @@ const jwt = require('jsonwebtoken');
 exports.getAllCustomers = (req, res) => {
     if (res.tokenRole == "ADMIN") {
         Customer.findAll({
-            order: [['createdAt', 'DESC']],
-            include: ["Roles", "Companies", "Buildings"]
+            include: [
+                {
+                    model: Building,
+                    through: {
+                        attributes: ['isOwner'],
+                    },
+                },
+                {
+                    model: Role,
+                    as: "Roles"
+                },
+                {
+                    model: Company,
+                    as: "Companies"
+                },
+                {
+                    model: Ticket,
+                    as: "Tickets"
+                },
+            ],
         })
-            .then(customers => res.json({ data: customers }))
-            .catch(err => res.status(500).json({ message: 'Database Error', error: err }))
+        .then(customers => res.json(customers))
+        .catch(err => res.status(500).json({ message: 'Database Error', error: err }))
     } else {
         return res.status(401).json({ message: 'Unauthorized' });
     }
@@ -20,19 +42,14 @@ exports.getAllCustomers = (req, res) => {
 
 exports.getCustomer = async (req, res) => {
     let customerId = parseInt(req.params.id)
-
-    // Vérification si le champ id est présent et cohérent
     if (!customerId) {
         return res.json(400).json({ message: 'Missing Parameter' })
     }
-
     try{
-        // Récupération de l'utilisateur et vérification
         let customer = await Customer.findOne({ include: ["Roles", "Companies"], where: { id: customerId }})
         if (customer === null) {
             return res.status(404).json({ message: 'This customer does not exist !' })
         }
-
         return res.json({ data: customer })
     }catch(err){
         return res.status(500).json({ message: 'Database Error', error: err })
@@ -40,22 +57,47 @@ exports.getCustomer = async (req, res) => {
 }
 
 exports.getCustomerRoleById = async (req, res) => {
-
-    // Vérification si le champ id est présent et cohérent
     if (!parseInt(res.tokenId)) {
         return res.json(400).json({ message: 'Missing token'})
     }
     try {
-        // Récupération de l'utilisateur et vérification
         let customer = await Customer.findOne({attributes: ["Roles.name"], include: ["Roles"], where: { id: parseInt(res.tokenId) }})
         if (customer === null) {
             return res.status(404).json({ message: 'This customer does not exist !' })
         }
-
         return res.json({ data: customer })
     }catch(err){
         return res.status(500).json({ message: 'Database Error', error: err })
-    }    
+    }
+}
+
+exports.getCustomerBuildings = async (req, res) => {
+    let customerId = parseInt(req.params.id)
+    if (!customerId) {
+        return res.json(400).json({ message: 'Missing Parameter' })
+    }
+
+    try {
+        const response = await Customer.findOne({
+            attributes: ['id', 'first_name', 'last_name', 'email', 'password','img', 'notifications', 'roles_id', 'companies_id', 'subscriptions_id'],
+            where: {
+                id: req.params.id,
+            },
+            include: {
+                model: Building,
+                attributes: ['id', 'name'],
+                through: {
+                    attributes: ['customerId', 'isOwner'],
+                },
+            },
+        });
+        res.status(200).json(response.Buildings)
+    } catch(err){
+        if(err.name == 'SequelizeDatabaseError'){
+            return res.status(500).json({ message: 'Database Error', error: err })
+        }
+        return res.status(500).json({ message: 'Hash Process Error', error: err})        
+    }
 }
 
 exports.addCustomer = async (req, res) => {
@@ -65,7 +107,6 @@ exports.addCustomer = async (req, res) => {
     if (!email || !password ) {
         return res.status(400).json({ message: 'Missing Data' })
     }
-
     try {
         // Vérification si l'utilisateur existe déjà
         const customer = await Customer.findOne({ where: { email: email }, raw: true })
@@ -73,12 +114,10 @@ exports.addCustomer = async (req, res) => {
             return res.status(409).json({ message: `The customer ${email} already exists !` })
         }
         const hash = await bcrypt.hash(password, 10);
-
         // Création de l'utilisateur
         let customerc = await Customer.create({...req.body, password: hash})
     
         return res.json({ message: 'Customer Created', data: { customerc } })
-
     }catch(err){
         if(err.name == 'SequelizeDatabaseError'){
             res.status(500).json({ message: 'Database Error', error: err })
@@ -89,26 +128,21 @@ exports.addCustomer = async (req, res) => {
 
 exports.updateCustomer = async (req, res) => {
     let customerId = parseInt(req.params.id);
-    
     // Vérification si le champ id est présent et cohérent
     if (!customerId) {
         return res.status(400).json({ message: 'Missing parameter' })
     }
-
     // Si le token contient bien un rôle et un token
     if (!res.tokenRole || !res.tokenId) {
         return res.status(400).json({ message: 'Missing token' })
     }
-    
     if (res.tokenRole == "ADMIN" || customerId === res.tokenId) {
         try{
-
             // Recherche de l'utilisateur et vérification
             let customer = await Customer.findOne({ where: {id: customerId}, raw: true})
             if(customer === null) {
                 return res.status(404).json({ message: 'This customer does not exist !' })
             }
-    
             // Mise à jour de l'utilisateur
             await Customer.update(req.body, { where: {id: customerId} })
             return res.json({ message: 'Customer Updated' })
@@ -122,19 +156,13 @@ exports.updateCustomer = async (req, res) => {
 
 exports.deleteCustomer = (req, res) => {
     let customerId = parseInt(req.params.id);
-
-    // Vérification si le champ id est présent et cohérent
     if (!customerId) {
         return res.status(400).json({ message: 'Missing parameter' })
     }
-
-    // Si le token contient bien un rôle et un token
     if (!res.tokenRole || !res.tokenId) {
         return res.status(400).json({ message: 'Missing token' })
     }
-
     if (res.tokenRole == "ADMIN" || customerId === res.tokenId) {
-        // Suppression de l'utilisateur
         Customer.destroy({ where: {id: customerId}, force: true})
             .then(() => res.status(200).json({ message: 'Customer deleted' }))
             .catch(err => res.status(500).json({ message: 'Database Error', error: err }))
@@ -145,14 +173,10 @@ exports.deleteCustomer = (req, res) => {
 
 exports.authenticateCustomer = async (req, res) => {
     const { email, password } = req.body;
-
-    // Validation des données reçues
     if (!email || !password ) {
         return res.status(400).json({ message: 'Missing Data' });
     }
-
     try {
-        // Vérification si l'utilisateur existe déjà
         const customer = await Customer.findOne({ include: ["Roles", "Companies"], where: { email: email }, raw: true })
         if (customer == null) {
             return res.status(404).json({ message: `Customer not found.` })
@@ -167,7 +191,6 @@ exports.authenticateCustomer = async (req, res) => {
                 }
             })
         }
-
     } catch(err) { 
         if(err.name == 'SequelizeDatabaseError'){
             res.status(500).json({ message: 'Database Error', error: err })
