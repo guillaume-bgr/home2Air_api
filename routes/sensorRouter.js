84bd0c6ab80681fe2ab3698d993e70e19b1a6f81
Cécilia Ruin <cecilia.ruin@gmail.com>
2023-03-10 11:47:23 +0100

    add seeders and sensors

--- routes/sensorRouter.js
+++ routes/sensorRouter.js
@@ -3,9 +3,10 @@ const authToken = require('../middleware/authToken');
 module.exports = (app) => {
     app.group("/sensors", (router) => {
         router.get("/", SensorController.getAllSensors); // FindAll
-        router.post('/create', SensorController.createSensor); // Create
-        router.patch('/:id', authToken, SensorController.updateSensor); // Update
-        router.delete('/:id', authToken, SensorController.deleteSensor); // Delete
+        router.post('/create', SensorController.addSensor); // Create
+        router.patch('/:id', SensorController.updateSensor); // Update
+        router.delete('/:id', SensorController.deleteSensor); // Delete
         router.get('/:id', SensorController.getSensor); // FindOne
+        router.get('/sensor-history/:id', SensorController.getSensorHistory);
     });
 }
